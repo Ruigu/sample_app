a967e14cd5a83ba3fd70ddbd3d45102d53208738
Ruigu <ruigukev@gmail.com>
2012-07-09 17:29:47 +0300

    layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/about"
+  match '/about', to: 'static_pages#about'
 
-  get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact'
 
-  get "static_pages/carrers"
+  match '/carrers', to: "static_pages#carrers"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
